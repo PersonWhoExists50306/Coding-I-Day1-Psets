8be79191a3bb537f4c383a58ad95c0532a3adf0e
PersonWhoExists50306 <107877092+PersonWhoExists50306@users.noreply.github.com>
2022-06-20 15:44:59 -0400

    Update conversion.py

--- Problem Sets/conversion.py
+++ Problem Sets/conversion.py
@@ -5,14 +5,12 @@
 """
 Background: 
     Convert user inputted hours and minutes into just minutes. Then, covert them into just hours.
-
 Your Mission: 
     Problem 0: 
         Take in input from the user for hours and minutes. Then, 
         we’ll use an integer to represent to total number of minutes 
         the user has entered after converting hours into minutes! 
         Lastly, we’ll print this value to the screen!
-
     Problem 1: 
        Take in input from the user for hours and minutes. Then, we want to 
        create a float for time in hours (where the digits to the left of the 
@@ -24,10 +22,12 @@ Your Mission:
 
 # Problem 0 
 print("Problem 0")
-# TODO: Replace this TODO with code for Problem 0  
-print()
+hours = int(input("How many full hours? "))
+minutes = int(input("How many extra minutes? "))
+totalMins = (60 * hours) + minutes
+print(f"{totalMins} minutes")
 
 # Problem 1 
 print("Problem 1")
-# Replace TODO with code for Problem 1 
-
+justHours = hours + (minutes / 60)
+print(f"{justHours} hours")
