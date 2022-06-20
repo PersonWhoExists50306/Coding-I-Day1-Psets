53b4b3197f3375e65febb436c472db6c7bd13c69
PersonWhoExists50306 <107877092+PersonWhoExists50306@users.noreply.github.com>
2022-06-20 14:05:09 -0400

    Update faces.py
    solved

--- Problem Sets/faces.py
+++ Problem Sets/faces.py
@@ -1,51 +1,47 @@
-                          # Faces 
-
 # Goal: Print some faces and shapes! 
 
 """
 Background: Let’s quickly play around with print! 
             To do so, let’s create python programs that print 
             the following faces and objects using only one print statement!
-
 Your Mission: 
     In a file called faces.py use only one print statement per 
     face to print the following!
-
 Face 0: 
 :P
-
 Face 1:
 ^  ^
 ____
-
 Face 2: 
 ' '
  .
 ~~~
-
 Face 3 (Gameboy): 
  ___
 |[_]|
 |+ ;|
 `---'
-
 Face 4 (Toilet): 
      _
     | |
  ___| |
 (    .'
  )  (   
-
 """
 
 # Code goes below! 
 
 # Face 0 
-
+print("Face 0:\n:P")
+print("\n")
 # Face 1 
-
+print("Face 1:\n^  ^\n____")
+print("\n")
 # Face 2 
-
+print("Face 2:\n' '\n .\n~~~")
+print("\n")
 # Gameboy 
-
-# Toilet 
+print("Gameboy:\n ___\n|[_]|\n|+ ;|\n`---'")
+print("\n")
+# Toilet
+print("Toilet:\n     _\n    | |\n ___| |\n(    .'\n )  (   ")
